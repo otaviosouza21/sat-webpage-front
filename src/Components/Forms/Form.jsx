9d8a69cd691a13e3a58c93c1c8ffb9fad3c4fb0f
unknown <otaviosouzalu@gmail.com>
2024-03-26 13:52:40 -0300

    defaults para desenvolvimento

---
+++ src/Components/Forms/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <form>Form</form>
+  )
+}
+
+export default Form
---
+++ src/Components/Home-Empreendedores/HomeEmpreendedores.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './HomeEmpreendedores.module.css'
+
+const HomeEmpreendedores = () => {
+  return (
+    <main>HomeEmpreendedores</main>
+  )
+}
+
+export default HomeEmpreendedores
