ce42f3ab33cb202586176df3765ed38559d0993e
Felipe Johnny <felipijohnny@outlook.com>
2024-04-15 14:35:24 -0300

    ajustando menuMobile e aplicando animacao de abertura e fechamento

--- src/Components/Header/NavLinksMobile/NavLinkMobile.jsx
+++ src/Components/Header/NavLinksMobile/NavLinkMobile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import menuBurguer from "../../../assets/icons/menu-burgues.svg";
 import closeIcon from "../../../assets/icons/close.svg";
 import style from "./NavLinkMobile.module.css";
@@ -9,22 +9,43 @@ const NavLinkMobile = ({ links }) => {
   const [menuMobile, setMenuMobile] = React.useState(false);
   const { userAuth, modal, setModal } = useContext(GlobalContext);
   const navigate = useNavigate();
+  const closeMenuBtn = useRef();
 
+
+function openMenuMobile(){
+  if (menuMobile) { 
+    closeMenuBtn.current.removeAttribute('class','animation-rigth-left') 
+    closeMenuBtn.current.setAttribute('class','animation-left-rigth') 
+  }
+  setMenuMobile(true)
+}
+
+function closeMenuMobile(){
+  if (closeMenuBtn.current) { 
+    closeMenuBtn.current.removeAttribute('class','animation-left-rigth') 
+    closeMenuBtn.current.setAttribute('class','animation-rigth-left') 
+  }
+  setTimeout(()=>{
+    setMenuMobile(false)
+  },500)
+
+}
   return (
     <div className={style.containerMenuMobile}>
       <img
-        onClick={() => setMenuMobile(true)}
+        onClick={openMenuMobile}
         className={style.menuBurguer}
         src={menuBurguer}
         alt=""
       />
       {menuMobile && (
-        <ul className={`${style.navLinksMobile} animation-left-rigth`}>
+        <ul className={`${style.navLinksMobile} animation-left-rigth`} ref={closeMenuBtn}>
           <img
-            onClick={() => setMenuMobile(false)}
+            onClick={closeMenuMobile}
             className={style.closeMenu}
             src={closeIcon}
             alt=""
+            
           />
           {!userAuth.status && (
             <li className={style.butonsMobile}>
