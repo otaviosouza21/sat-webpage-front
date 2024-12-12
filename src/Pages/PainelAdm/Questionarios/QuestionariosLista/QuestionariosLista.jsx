9e056d2a11c3e0bedf6ae23f850d67d21c6248e2
otaviosouza21 <otaviosouzalu@gmail.com>
2024-12-11 22:35:00 -0300

    refatorando formularios

--- src/Pages/PainelAdm/Questionarios/QuestionariosLista/QuestionariosLista.jsx
+++ src/Pages/PainelAdm/Questionarios/QuestionariosLista/QuestionariosLista.jsx
@@ -5,14 +5,13 @@ import styles from "./QuestionarioLista.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import useFetch from "../../../../Hooks/useFetch";
 import { DELETE_DATA, GET_ALL } from "../../../../Api/api";
-import { convertData } from "../../../../plugins/convertData";
-import TrashIcon from "../../../../assets/svgFlies/TrashIcon";
-import PenIcon from "../../../../assets/svgFlies/PenIcon";
 import useTokenValidate from "../../../../Hooks/useTokenValidate";
 import useToast from "../../../../Hooks/useToast";
-import LoadingDots from "../../../../Components/Utils/LoadingDots/LoadingDots";
+
 import LoadingCenterComponent from "../../../../Components/Utils/LoadingCenterComponent/LoadingCenterComponent";
 import { GlobalContext } from "../../../../Hooks/GlobalContext";
+import QuestionarioCardList from "./QuestionarioCardList";
+import HeaderQuestionarioLista from "./HeaderQuestionarioLista";
 
 const QuestionariosLista = () => {
   const { request, loading, error } = useFetch();
@@ -59,50 +58,17 @@ const QuestionariosLista = () => {
   if (error) return <p>Não foi possivel carregar dados</p>;
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <Title text="Questionários" fontSize="3" />
-        <Link
-          to={"/questionario/cadastro"}
-          onClick={() => setDataUpdate('')}
-          className={styles.button}
-        >
-          <img src={Plus} alt="" />
-          Novo Questionário
-        </Link>
-      </div>
+      <HeaderQuestionarioLista />
       <ul className={styles.cards}>
         {formulariosData.length > 0 ? (
-          formulariosData.map((form, index) => {
+          formulariosData.map((form) => {
             return (
-              <li key={index} className={styles.card}>
-                <div className={styles.header}>
-                  <h4>{form.titulo}</h4>
-                  <span
-                    style={
-                      form.status ? { color: "green" } : { color: "tomato" }
-                    }
-                  >
-                    {form.status ? "Ativo" : "Inativo"}
-                  </span>
-                </div>
-                <p>{form.descricao}</p>
-                <div className={styles.data}>
-                  <span>Inicio: {convertData(form.vigencia_inicio)}</span>
-                  <span>Fim: {convertData(form.vigencia_fim)}</span>
-                </div>
-                <div className={styles.icons}>
-                  <TrashIcon
-                    onclick={() => handleDelete(form.id)}
-                    color={"green"}
-                    size="30px"
-                  />
-                  <PenIcon
-                    onclick={() => handleEdit(form)}
-                    color={"green"}
-                    size="30px"
-                  />
-                </div>
-              </li>
+              <QuestionarioCardList
+                key={form.id}
+                form={form}
+                handleDelete={handleDelete}
+                handleEdit={handleEdit}
+              />
             );
           })
         ) : (
