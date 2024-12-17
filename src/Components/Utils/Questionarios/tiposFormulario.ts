1eeeeaafe04d2b59dca8cfa3d967f846fb3e1650
Felipe Johnny <felipijohnny@outlook.com>
2024-12-17 08:04:20 -0300

    aplicando novo arquivo de tipos de formularios

---
+++ src/Components/Utils/Questionarios/tiposFormulario.ts
@@ -0,0 +1,29 @@
+export const tiposPergunta = [
+  {
+    id: 1,
+    nome: "Texto",
+  },
+  {
+    id: 2,
+    nome: "Multipla escolha",
+  },
+];
+
+export const tipoFormulario = [
+  {
+    id: 1,
+    nome: "Cadastro de serviço",
+    descrição: "formulário ativará ao tentar cadatrar um novo serviço",
+  },
+  {
+    id: 2,
+    nome: "Pagina Inicial - usuario logado",
+    descrição: "formulário ativará ao entrar na pagina principal",
+  },
+  {
+    id: 3,
+    nome: "Pagina Inicial - usuario deslogado",
+    descrição:
+      "formulário ativará ao entrar na pagina principal (quando não logado)",
+  },
+];
